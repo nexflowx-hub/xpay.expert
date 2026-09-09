23ea24957c7aefaca32272f688ab963f6c13d803
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-08 22:16:33 -0300

    feat: add expert ops intake page

---
+++ src/app/ops/intake/page.tsx
@@ -0,0 +1,11 @@
+import '../ops.css';
+import OpsIntakeClient from '@/components/ops-intake-client';
+
+export const metadata = {
+  title: 'Documentos & Comprovativos | XPay Expert',
+  robots: { index: false, follow: false }
+};
+
+export default function OpsIntakePage(){
+  return <main className="portal"><OpsIntakeClient /></main>;
+}
